50a987effc2d23f15f716bd2994b9f840cba3135
Max van Hattum <438466@student.fontys.nl>
2021-03-30 10:03:56 +0200

    Extracted methods, cleaned up names

--- poc-topic-analysis/TopicModelling-v1.py
+++ poc-topic-analysis/TopicModelling-v1.py
@@ -8,9 +8,29 @@ import nltk
 from nltk.corpus import stopwords
 import gensim.corpora as corpora
 from tfidf import dfItf
-import pyLDAvis
 import pyLDAvis.gensim_models
 
+
+def remove_whitespace_and_punct(articles_df, column_to_make, column_input):
+    articles_df[column_to_make] = articles_df[column_input].map(lambda x: re.sub('[,\.!?]', '', x))
+    articles_df[column_to_make] = articles_df[column_to_make].map(lambda x: x.lower())
+
+
+def create_wordcloud_to_png(articles_df, column, file_name):
+    long_string = ','.join(list(articles_df[column].values))
+    wordcloud = WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color='steelblue')
+    wordcloud.generate(long_string)
+    wordcloud.to_file(file_name)
+
+
+def sent_to_words(sentences):
+    for sentence in sentences:
+        yield gensim.utils.simple_preprocess(str(sentence), deacc=True)
+
+
+def remove_stopwords(text_stopwords):
+    return [[word for word in simple_preprocess(str(doc))
+             if word not in stop_words] for doc in text_stopwords]
 
 
 if __name__ == '__main__':
@@ -26,62 +46,32 @@ if __name__ == '__main__':
 
     articlesDF = pd.DataFrame(articlesListOfDicts)
 
+    remove_whitespace_and_punct(articlesDF, 'title_processed', 'title')
+    remove_whitespace_and_punct(articlesDF, 'content_processed', 'content')
 
-
-    articlesDF['title_processed'] = articlesDF['title'].map(lambda x: re.sub('[,\.!?]', '', x))
-    articlesDF['title_processed'] = articlesDF['title_processed'].map(lambda x: x.lower())
-
-    articlesDF['content_processed'] = articlesDF['content'].map(lambda x: re.sub('[,\.!?]', '', x))
-    articlesDF['content_processed'] = articlesDF['content_processed'].map(lambda x: x.lower())
-
-
-    long_string = ','.join(list(articlesDF['content_processed'].values))
-    wordcloud = WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color='steelblue')
-    wordcloud.generate(long_string)
-    wordcloud.to_file("wordcloud-content.png")
-    long_string = ','.join(list(articlesDF['title_processed'].values))
-    wordcloud = WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color='steelblue')
-    wordcloud.generate(long_string)
-    wordcloud.to_file("wordcloud-title.png")
+    create_wordcloud_to_png(articlesDF, 'content_processed', 'wordcloud-content.png');
+    create_wordcloud_to_png(articlesDF, 'title_processed', 'wordcloud-title.png');
 
     stop_words = stopwords.words('english')
     stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
 
-
-    def sent_to_words(sentences):
-        for sentence in sentences:
-            yield gensim.utils.simple_preprocess(str(sentence), deacc=True)
-
-
-    def remove_stopwords(texts):
-        return [[word for word in simple_preprocess(str(doc))
-                 if word not in stop_words] for doc in texts]
-
-
     data = articlesDF.content_processed.values.tolist()
     data_words = list(sent_to_words(data))
     data_words = remove_stopwords(data_words)
-
     scoredDocs = dfItf(data_words)
-
     id2word = corpora.Dictionary(scoredDocs)
     texts = scoredDocs
-
     corpus = [id2word.doc2bow(text) for text in texts]
-
-
 
     num_topics = 5
     lda_model = gensim.models.LdaMulticore(corpus=corpus,
                                            id2word=id2word,
                                            num_topics=num_topics)
-    # Print the Keyword in the 10 topics
     topics = lda_model.print_topics()
     print(topics)
     lda_models = lda_model[corpus[0]]
 
-    print("iets")
 
     visualisation = pyLDAvis.gensim_models.prepare(lda_model, corpus, id2word)
-    pyLDAvis.save_html(visualisation, './test- ' + str(num_topics) + '.html')
+    pyLDAvis.save_html(visualisation, './test-' + str(num_topics) + '.html')
 
