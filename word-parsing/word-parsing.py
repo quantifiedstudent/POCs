06d3e501eeeae6432079dfae5a21f6896e150677
Ruben <rubenfricke@gmail.com>
2021-04-19 11:28:51 +0200

    Convert docx to txt using pypandoc

--- word-parsing/word-parsing.py
+++ word-parsing/word-parsing.py
@@ -1,10 +1,30 @@
+# ===================================================
+# DOCUMENT FROM DOCX
+# ===================================================
+
 from docx import Document
 
 doc = Document(".\data\document-1.docx")
 paragraphs = doc.paragraphs
 print(paragraphs)
 
+file = open("output-docx.txt", "w+")
+
 for par in paragraphs:
     text = par.text
     if len(text) > 0:
         print(text)
+        file.write(text + "\n")
+
+file.close()
+
+
+
+# ===================================================
+# PYPANDOC
+# ===================================================
+
+import pypandoc
+
+output = pypandoc.convert_file("./data/document-1.docx", "plain", outputfile="output-pypandoc.txt")
+assert output == ""
