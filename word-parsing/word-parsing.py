d3d86a2735d595e12183944a1df9f9708a99abaa
Ruben <rubenfricke@gmail.com>
2021-04-19 11:43:19 +0200

    Parser using groupdocs api (also with pdf parsing included)

--- word-parsing/word-parsing.py
+++ word-parsing/word-parsing.py
@@ -28,3 +28,29 @@ import pypandoc
 
 output = pypandoc.convert_file("./data/document-1.docx", "plain", outputfile="output-pypandoc.txt")
 assert output == ""
+
+
+
+# ===================================================
+# GROUPDOCS API
+# ===================================================
+
+import groupdocs_conversion_cloud
+from shutil import copyfile
+
+client_id = "ENTER CLIENT ID HERE"
+client_key = "ENTER CLIENT SECRET HERE"
+
+convert_api = groupdocs_conversion_cloud.ConvertApi.from_keys(client_id, client_key)
+
+try:
+    request = groupdocs_conversion_cloud.ConvertDocumentDirectRequest("txt", "./data/document-1.docx")
+    result = convert_api.convert_document_direct(request)
+    copyfile(result, 'output-groupdocs.txt')
+
+    request = groupdocs_conversion_cloud.ConvertDocumentDirectRequest("txt", "./data/document-1.pdf")
+    result = convert_api.convert_document_direct(request)
+    copyfile(result, 'output-groupdocs-pdf.txt')
+
+except groupdocs_conversion_cloud.ApiException as e:
+    print("Exception when calling get_supported_conversion_types: {0}".format(e.message))
